f4e9ee7c2ef0ec625aff0a0efd484009afc6e9d6
aldlflcl <64316701+aldlflcl@users.noreply.github.com>
2023-09-28 10:53:36 +0900

    [Bronze II] Title: 방 배정, Time: 80 ms, Memory: 6184 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.cs
+++ 백준/Bronze/13300. 방 배정/방 배정.cs
@@ -3,23 +3,32 @@ int n = int.Parse(input[0]);
 int k = int.Parse(input[1]);
 int[] male = new int[7];
 int[] female = new int[7];
+int solve = 0;
 for (int i = 0; i < n; i++)
 {
     string[] student = Console.ReadLine().Split();
-    if (student[0] == "0") female[int.Parse(student[1])]++;
-    else male[int.Parse(student[1])]++;
+    int grade = int.Parse(student[1]);
+    if (student[0] == "0")
+        female[grade]++;
+    else
+        male[grade]++;
 }
-int solve = 0;
+
 for (int i = 1; i < 7; i++)
 {
     if (male[i] > k)
     {
-        solve += male[i] / k + male[i] % k;
-    } else if (male[i] > 0) solve++;
+        int rest = male[i] % k;
+        solve += male[i] / k + (rest > 0 ? 1 : 0);
+    }
+    else if (male[i] > 0) solve++;
 
     if (female[i] > k)
     {
-        solve += female[i] / k + female[i] % k;
-    } else if (female[i] > 0) solve++;
+        int rest = female[i] % k;
+        solve += female[i] / k + (rest > 0 ? 1 : 0);
+    }
+    else if (female[i] > 0) solve++;
 }
+
 Console.WriteLine(solve);
